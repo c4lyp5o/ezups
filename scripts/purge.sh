92e2c985cf951d5e2456d773826be0db53531816
c4lyp5o <calypso@calypsocloud.one>
2025-05-17 01:41:29 +0800

    chore: delete unused

--- scripts/purge.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/ash
-
-curl --location --request POST 'http://localhost:3000/api/purge' \
---header 'Content-Type: application/json' \
---data-raw '{
-    "API_KEY": "<paste your api key here>"
-}'
